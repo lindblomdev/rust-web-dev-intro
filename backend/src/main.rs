9e133357c5b6543b85dc77d813397f34017d0101
Christopher Lindblom <lindblom0@outlook.com>
2024-01-04 22:13:10 +0100

    Add vue and wire up concurrently

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,17 @@
 use axum::{
-    Json, Router, routing
+    http::{header::CONTENT_TYPE, HeaderValue, Method},
+    routing, Json, Router,
 };
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", routing::get(handler));
+    let app = Router::new().route("/", routing::get(handler)).layer(
+        tower_http::cors::CorsLayer::new()
+            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+            .allow_headers([CONTENT_TYPE])
+            .allow_methods([Method::GET]),
+    );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Server started, listening on {addr}");
