0d479cdd71710eb3c13be7c74e60ec9489969a49
Christopher Lindblom <lindblom0@outlook.com>
2024-01-04 22:01:11 +0100

    Add axum foundation

---
+++ backend/src/main.rs
@@ -0,0 +1,28 @@
+use axum::{
+    Json, Router, routing
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", routing::get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    println!("Server started, listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+#[derive(serde::Serialize)]
+struct Message {
+    message: String,
+}
+
+async fn handler() -> Json<Message> {
+    Json(Message {
+        message: String::from("Hello, World!"),
+    })
+}
